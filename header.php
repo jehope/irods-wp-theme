6aec8e0785a8a8f348a33c37e5939a7ec93aed59
jehope <jehope2@gmail.com>
2014-03-27 18:52:12 -0400

    Fixed title problem and meta tag validation errors

--- header.php
+++ header.php
@@ -8,14 +8,14 @@
 	<head>
 		<meta charset="utf-8">
 
-		<title><?php wp_title(''); ?></title>
+		<title><?php bloginfo('name'); ?></title>
 
 		<!-- Google Chrome Frame for IE -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 		<!-- mobile meta -->
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
+		<meta name="HandheldFriendly" content="True" />
+		<meta name="MobileOptimized" content="320" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 		<!-- icons & favicons -->
